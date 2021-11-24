e2a5c1bdcacd9e0dc3d12f1e549b8ddbb9c99c2e
Areej Fatima <areej4761@gmail.com>
2021-11-24 22:39:08 +0500

    first commit

---
+++ assignment4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace question_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           const  float PI = 3.124F;
+            Console.WriteLine(" Value of PI " + PI);
+            Console.ReadLine();
+        }
+    }
+}
